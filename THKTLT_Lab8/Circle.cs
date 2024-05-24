8e84d37c096be24b9e75d7dc8b7d99a6bff099c6
Useless Laptop <Useless Laptop@DESKTOP-GU330FV>
2024-05-24 21:57:27 +0700

    Add project files.

---
+++ THKTLT_Lab8/Circle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+
+namespace THKTLT_Lab8
+{
+    class Circle: IDraw, IColor
+    {
+        private int diameter;
+        private int locX = 0, locY = 0;
+        private Ellipse circle = null;
+        public Circle(int diameter)
+        {
+            this.diameter = diameter;
+        }
+        void IDraw.SetLocation(int xCoord, int yCoord)
+        {
+            this.locX = xCoord;
+            this.locY = yCoord;
+        }
+        void IDraw.Draw(Canvas canvas)
+        {
+            if (this.circle != null)
+            {
+                canvas.Children.Remove(this.circle);//Xóa đối tượng
+            }
+            else
+            {
+                this.circle = new Ellipse(); //Tạo một đối tượng mới
+            }
+            this.circle.Height = this.diameter; //Điều chỉnh chiều cao
+            this.circle.Width = this.diameter; //Điều chỉnh chiều rộng
+            Canvas.SetTop(this.circle, this.locY); //Set vị trí cạnh trên của đoi tượng
+            Canvas.SetLeft(this.circle, this.locX);//Set vị trí cạnh trái của đối tượng
+            canvas.Children.Add(this.circle); //Thêm vào Canvas
+
+        }
+        void IColor.SetColor(Color color)
+        {
+            if (this.circle != null)
+            {
+                SolidColorBrush brush = new SolidColorBrush(color);
+                this.circle.Fill = brush;
+
+            }
+        }
+    }
+}
---
+++ THKTLT_Lab8/Square.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+
+namespace THKTLT_Lab8
+{
+    class Square: IDraw, IColor
+    {
+        private int sideLength; //Cạnh của hình
+        private int locX = 0, locY = 0; //Tạo độ cần vẽ
+        private Rectangle rect = null; //Dùng để tạo hình chữ nhật
+
+        public Square(int sideLength)
+        {
+            this.sideLength = sideLength;
+        }
+
+        void IDraw.Draw(Canvas canvas)
+        {
+            if (this.rect != null)
+            {
+                canvas.Children.Remove(this.rect);//Xóa đối tượng
+            }
+            else
+            {
+                this.rect = new Rectangle(); //Tạo một đối tượng mới
+            }
+            this.rect.Height = this.sideLength; //Điều chỉnh chiều cao
+            this.rect.Width = this.sideLength; //Điều chỉnh chiều rộng
+            Canvas.SetTop(this.rect, this.locY); //Set vị trí cạnh trên của đoi tượng
+            Canvas.SetLeft(this.rect, this.locX);//Set vị trí cạnh trái của đối tượng
+            canvas.Children.Add(this.rect); //Thêm vào Canvas
+
+        }
+
+        void IColor.SetColor(Color color)
+        {
+            if (this.rect != null)
+            {
+                SolidColorBrush brush = new SolidColorBrush(color);
+                this.rect.Fill = brush;
+
+            }
+        }
+        void IDraw.SetLocation(int xCoord, int yCoord)
+        {
+            this.locX = xCoord;
+            this.locY = yCoord;
+        }
+    }
+}
